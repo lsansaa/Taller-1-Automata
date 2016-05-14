f886c6bdd937ef67498945b814e0f4d632fbd493
lsansaa <lsansaa@gmail.com>
2016-05-13 21:23:53 -0300

    Pablito clavo un clavito2

--- src/taller_automatas/ConversionNFAtoDFA.java
+++ src/taller_automatas/ConversionNFAtoDFA.java
@@ -23,32 +23,49 @@ public class ConversionNFAtoDFA {
         this.DFA = new Automata();  
         this.cantEstados = NFA.listaEstados.size();
         this.cantAlfabeto = NFA.alfabeto.size();
-//        Conversion(NFA);
+        Conversion(NFA);
 //        System.out.println(this.cantEstados);
 //        System.out.println(this.cantAlfabeto);
     }
     
     private void Conversion(Automata NFA){
         
-        String[][] matrizNFA = new String[this.cantAlfabeto+1][this.cantEstados];
+        String[][] matrizNFA = new String[this.cantEstados][this.cantAlfabeto+1];
         
-        for(int i=0;i <this.cantAlfabeto; i++){
-            String caracter = NFA.alfabeto.get(i);
-            String[] aux;
-            String[] aux2;
-            for(int j=0;j <this.cantEstados; j++){
-                String estado = NFA.listaEstados.get(j);
+        for(int i=0;i <this.cantEstados; i++){
+            String estado = NFA.listaEstados.get(i);
+            for(int j=0;j <this.cantAlfabeto+1; j++){
+                String caracter;
+                if(j==this.cantAlfabeto){
+                    caracter = "epsilon";
+                }else{  
+                    caracter = NFA.alfabeto.get(j);
+                }
+                String[] aux;
+                String[] aux2;
                 for(String trans:NFA.listaTransiciones){
                     aux = trans.split(":");
                     if(aux[0].equals(estado)){
-                        aux2 = aux[1].split("-");
+                        aux2 = aux[1].split("–");
+                        aux2[0] = aux2[0].replaceAll("\\s", "");
+                        aux2[1] = aux2[1].replaceAll("\\s", "");
                         if(aux2[0].equals(caracter)){
                             matrizNFA[i][j] = aux2[1];
+                            if(caracter.equals("epsilon")){
+                                if(matrizNFA[i][j].equals(null)){
+                                    matrizNFA[i][j] = estado+","+aux2[1];
+                                }else{
+                                    matrizNFA[i][j] = matrizNFA[i][j]+","+estado+","+aux2[1];
+                                }
+                            }
                         }
                     }
                 }
                 if(matrizNFA[i][j]==null) {
                     matrizNFA[i][j]="-";
+                    if(caracter.equals("epsilon")){
+                                matrizNFA[i][j] = estado;
+                    }
                 }
                 System.out.print(matrizNFA[i][j]+" ");
             }
